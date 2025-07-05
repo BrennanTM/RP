b53baaa0f3400d51e1964c8f26c2e675ccb3c475
Tristan Brennan <brennantm1026@gmail.com>
2025-07-04 17:35:12 -0700

    Add files via upload

---
+++ eligibility/run_multi_survey.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Run the multi-survey processor in continuous mode"""
+import sys
+import time
+from multi_survey_processor import main
+
+# Simulate selecting option 4 (continuous mode)
+class ContinuousInput:
+    def __init__(self):
+        self.returned_4 = False
+        
+    def __call__(self, prompt):
+        if not self.returned_4:
+            self.returned_4 = True
+            return '4'
+        time.sleep(60)
+        return ''
+
+# Replace input with our automated version
+import builtins
+builtins.input = ContinuousInput()
+
+if __name__ == "__main__":
+    main()
